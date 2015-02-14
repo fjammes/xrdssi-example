f8b3f4dbc42de911f3819adc4bead81cd1cbb3ad
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-02-14 21:25:51 +0100

    Move client code to src/client

--- build-client.sh
+++ build-client.sh
@@ -1,5 +1,6 @@
 INC=$XROOTD_DIR/include/xrootd
 LIB=$XROOTD_DIR/lib
+SRC_DIR=./src/client/
 
-g++ -g -I. -I$INC -L$LIB -Llibcl -lXrdSsi -o ./bin/ssicl XrdSsiClMain.cc XrdSsiClUI.cc
+g++ -g -I. -I$INC -L$LIB -Llibcl -lXrdSsi -o ./bin/ssicl $SRC_DIR/XrdSsiClMain.cc $SRC_DIR/XrdSsiClUI.cc
 #g++ -g -I. -Isrc -Llib -lXrdUtils -shared -fPIC -o lib/myservice.so myservice.cc
