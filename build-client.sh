4036987ce310764e93849e815a28993f8e8e57f0
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-02-15 08:48:01 +0100

    Move service code to src/service

--- build-client.sh
+++ build-client.sh
@@ -1,6 +1,11 @@
+set -e
+set -x
+
 INC=$XROOTD_DIR/include/xrootd
 LIB=$XROOTD_DIR/lib
 SRC_DIR=./src/client/
+OUT_DIR=bin
 
-g++ -g -I. -I$INC -L$LIB -Llibcl -lXrdSsi -o ./bin/ssicl $SRC_DIR/XrdSsiClMain.cc $SRC_DIR/XrdSsiClUI.cc
-#g++ -g -I. -Isrc -Llib -lXrdUtils -shared -fPIC -o lib/myservice.so myservice.cc
+mkdir -p $OUT_DIR
+
+g++ -g -I$SRC_DIR -I$INC -L$LIB -Llibcl -lXrdSsi -o $OUT_DIR/ssicl $SRC_DIR/XrdSsiClMain.cc $SRC_DIR/XrdSsiClUI.cc
--- build-server.sh
+++ build-server.sh
@@ -1,1 +1,13 @@
-g++ -g -I. -Isrc -Llib -shared -fPIC -o libSsiSv.so XrdSsiSvService.cc XrdSsiSvSession.cc XrdSsiSvStreamActive.cc XrdSsiSvStreamPassive.cc
+set -e
+set -x
+
+INC=$XROOTD_DIR/include/xrootd
+LIB=$XROOTD_DIR/lib
+SRC_DIR=./src/server
+OUT_DIR=lib
+
+mkdir -p $OUT_DIR
+
+g++ -g -I$SRC_DIR -I$INC -L$LIB -shared -fPIC -o $OUT_DIR/libSsiSv.so \
+$SRC_DIR/XrdSsiSvService.cc $SRC_DIR/XrdSsiSvSession.cc \
+$SRC_DIR/XrdSsiSvStreamActive.cc $SRC_DIR/XrdSsiSvStreamPassive.cc
